4216bc5c7a35f300c1b66acac9249fc41f3cc52f
Maxx Sanner <maxxsanner105@gmail.com>
2020-09-10 21:04:29 -0700

    added image and some style

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,5 @@ INSERT INTO burgers (burger_name, devoured)
 VALUES ("Blue Cheeseburger", FALSE);
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Veggie Burger", FALSE);
+
+SELECT * FROM burgers

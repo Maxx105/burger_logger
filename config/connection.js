74f78724e77e8789a2259fbc34d651946e3a4b44
Maxx Sanner <maxxsanner105@gmail.com>
2020-09-13 16:26:04 -0700

    removed jawsDB credentials from connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,6 @@
 var mysql = require("mysql");
 var connection;
 
-var jawsDBHost = "d1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-var jawsDBUser = "t1jfthwc2450mfig";
-var jawsDBPassword = "n7axlzl90ns1b9hz";
-var jawsDBDatabase = "rw3id7ay13li50x6";
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
